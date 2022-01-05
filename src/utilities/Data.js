495860daf7110141d5f7a86694eeb904649fd243
Rustam Yuburov <rt138328@gmail.com>
2022-01-05 18:31:00 +0600

    Added data about projects

---
+++ src/utilities/Data.js
@@ -0,0 +1,44 @@
+const Data = [
+  {
+    title: 'Games Warehouse | Inventory Application',
+    summary:
+      'A backend focused Game store app that allows users to add, delete or update new games, genres and developers to database. Users can add images and also create new accounts.',
+    stack: 'Node.js, Express, Mongoose, EJS, Multer',
+    source: 'https://github.com/RustamYuburov/inventory-application',
+    demo: 'https://radiant-coast-44842.herokuapp.com/',
+    image:
+      'https://user-images.githubusercontent.com/66270461/144750620-72253e42-1897-476b-a50b-f227c1ae9d7c.png',
+  },
+  {
+    title: 'Book Planet | Shopping Cart',
+    summary:
+      'A React based web app for shopping books online. Users can choose books in shop and add them to cart.',
+    stack: 'Javascript, React, Styled Components, React Router, Framer Motion',
+    source: 'https://github.com/RustamYuburov/shopping-cart',
+    demo: 'https://rustamyuburov.github.io/shopping-cart/',
+    image:
+      'https://user-images.githubusercontent.com/66270461/131250287-6926296a-346b-4da4-b484-da94a4f74a38.png',
+  },
+  {
+    title: 'Zoo Forum | Members Only',
+    summary:
+      "A backend focused app that allows users to sign up, log in and add new messages to the database. Registerd users also have different level of permission. Non-Authorised and simple users can't see message's author and date. Members and Admin can see it.",
+    stack: 'Node.js, Express, MongoDB, Mongoose, Passport.js, Heroku',
+    source: 'https://github.com/RustamYuburov/zoo-forum',
+    demo: 'https://floating-sands-68804.herokuapp.com/',
+    image:
+      'https://user-images.githubusercontent.com/66270461/146673253-5b534889-9614-4f26-80e3-aa861092a9a8.png',
+  },
+  {
+    title: 'Memory cards with Pokemons',
+    summary:
+      "A simple game that is created using React. Application fetches data and creates pokemon cards. Get points for clicking on card, but you can't click on one card twice!",
+    stack: 'Javascript, React, Styled Components, React Icons, Pokeapi JS Wrapper',
+    source: 'https://github.com/RustamYuburov/memory-card',
+    demo: 'https://rustamyuburov.github.io/memory-card/',
+    image:
+      'https://user-images.githubusercontent.com/66270461/131547775-45fbe6f8-1378-4e83-86b9-550cabd6746e.png',
+  },
+];
+
+export default Data;
