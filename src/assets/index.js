07c444379570289c66bdf66790d41b6af84fdae7
Rustam Yuburov <rt138328@gmail.com>
2022-01-05 18:28:03 +0600

    Added images and index.js file to export them

---
+++ src/assets/index.js
@@ -0,0 +1,45 @@
+import Backend from './backend.png';
+import Frontend from './frontend.png';
+import Miscellaneous from './miscellaneous.png';
+import Nodejs from './nodejs.png';
+import EJS from './ejs.png';
+import Expressjs from './expressjs.png';
+import Mongodb from './mongodb.png';
+import Mongoose from './mongoose.png';
+import HTML from './html5.png';
+import CSS from './css3.png';
+import Javascript from './javascript.png';
+import ReactImg from './react.png';
+import StyledComponents from './styled-components.svg';
+import Git from './git.png';
+import Github from './github.png';
+import Heroku from './heroku.png';
+import Webpack from './webpack.png';
+import NPM from './npm.png';
+import Russian from './russia.png';
+import English from './united-kingdom.png';
+import Chinese from './china.png';
+
+export {
+  Backend,
+  Frontend,
+  Miscellaneous,
+  Nodejs,
+  EJS,
+  Expressjs,
+  Mongodb,
+  Mongoose,
+  HTML,
+  CSS,
+  StyledComponents,
+  Javascript,
+  ReactImg,
+  Git,
+  Github,
+  Heroku,
+  Webpack,
+  NPM,
+  Russian,
+  English,
+  Chinese
+};
