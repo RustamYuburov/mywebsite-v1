16d8ad67e140e9f112ccbb07df94b4e563e896c1
Rustam Yuburov <rt138328@gmail.com>
2022-01-10 17:02:42 +0600

    Changed work email in contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,13 +11,13 @@ const Contact = () => {
 
       <div className="contacts">
         <div className="contactInfo">
-          <a href="mailto:rt138328@gmail.com">
+          <a href="mailto:rustamyuburov.work@gmail.com">
             <img
               className="contactImg"
               src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/gmail.svg"
               alt="contact logo"
             />
-            <h3>rt138328@gmail.com</h3>
+            <h3>rustamyuburov.work@gmail.com</h3>
           </a>
         </div>
         <div className="contactInfo">
